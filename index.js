c5d352e33ecd546fa944f0c5a7007a3699442cd3
RMSilence <109949972+RMSilence@users.noreply.github.com>
2022-08-15 17:22:39 +0430

    new one

--- index.js
+++ index.js
@@ -5,17 +5,20 @@ function clock(){
 
   const date= new Date();
   
-const hours=((date.getHours()+11)%12+1);
+const hours=(date.getHours()+11)%12+1;
 const minutes=date.getMinutes();
 const seconds=date.getSeconds();
 
 const hour=hours*30;
 const minute=minutes*6;
 const second=seconds*6;
+console.log(second);
+document.querySelector('.hour').style.transform = `rotate(${hour}deg)`;
+document.querySelector('.minute').style.transform =`rotate(${minute}deg)`;
+document.querySelector('.second').style.transform =`rotate(${second}deg)`;
+}
 
-const hourClass=document.querySelector('hour')
-const minuteClass=document.quarySelector('minute')
-const secondClass=document.quarySelector('second')
+setInterval(clock,inc);
 
 
 }
